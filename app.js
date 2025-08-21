0a85b7d26eeb5affcc266e99e188ca2978fdf020
Francis <brakay1136@gmail.com>
2025-08-21 10:51:08 -0700

    api call done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 class GitHubAPI {
   constructor(username) {
-    this.username = username;
+    this.username = username.value;
   }
 
   async getData() {
@@ -13,15 +13,54 @@ class GitHubAPI {
 
   displayData() {
     this.getData().then((userProfile) => {
-      console.log(userProfile.data.login);
+      document.getElementById("ghAvatar").src = userProfile.data.avatar_url;
+      document.getElementById("ghName").textContent = userProfile.data.login;
+      document.getElementById("ghBio").textContent = userProfile.data.bio;
+      document.getElementById("ghReposCount").textContent =
+        userProfile.data.public_repos;
+      document.getElementById("ghFollowers").textContent =
+        userProfile.data.followers;
+      document.getElementById("ghFollowing").textContent =
+        userProfile.data.following;
     });
   }
 }
 
-const userProfile = new GitHubAPI("Francis-Frimpong");
-userProfile.displayData();
+class GitHubRepo extends GitHubAPI {
+  constructor(repo_container, username) {
+    super(username);
+    this.repo_container = repo_container;
+  }
 
-// axios
-//   .get("https://api.github.com/users/Francis-Frimpong")
-//   .then((res) => console.log(res.data))
-//   .catch((err) => console.error(err));
+  async getRepoData() {
+    const response = await axios(
+      `https://api.github.com/users/${this.username}/repos`
+    );
+
+    return response;
+  }
+
+  displayRepoData() {
+    this.repo_container.innerHTML = "";
+    this.getRepoData().then((userProfile) => {
+      userProfile.data.forEach((repo) => {
+        const li = document.createElement("li");
+        li.textContent = repo.name;
+        this.repo_container.appendChild(li);
+      });
+    });
+  }
+}
+
+const repoContainer = document.getElementById("ghRepos");
+const form = document.getElementById("ghForm");
+const username = document.getElementById("ghUser");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userProfile = new GitHubAPI(username);
+  const userRepo = new GitHubRepo(repoContainer, username);
+  userRepo.displayRepoData();
+  userProfile.displayData();
+  username.value = "";
+});
