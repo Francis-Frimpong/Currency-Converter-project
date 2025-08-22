e866a1b098b0711382244eacf384ec40e68f6eca
Francis <brakay1136@gmail.com>
2025-08-22 09:12:32 -0700

    app done

--- app.js
+++ app.js
@@ -8,22 +8,32 @@ class GitHubAPI {
       `https://api.github.com/users/${this.username}`
     );
 
-    return response;
+    return response.data;
   }
 
   displayData() {
-    this.getData().then((userProfile) => {
-      document.getElementById("ghError").innerHTML = "";
-      document.getElementById("ghAvatar").src = userProfile.data.avatar_url;
-      document.getElementById("ghName").textContent = userProfile.data.login;
-      document.getElementById("ghBio").textContent = userProfile.data.bio;
-      document.getElementById("ghReposCount").textContent =
-        userProfile.data.public_repos;
-      document.getElementById("ghFollowers").textContent =
-        userProfile.data.followers;
-      document.getElementById("ghFollowing").textContent =
-        userProfile.data.following;
-    });
+    this.getData()
+      .then((userProfile) => {
+        document.getElementById("ghError").innerHTML = "";
+        document.getElementById("ghAvatar").src = userProfile.avatar_url;
+        document.getElementById("ghName").textContent = userProfile.login;
+        document.getElementById("ghBio").textContent = userProfile.bio;
+        document.getElementById("ghReposCount").textContent =
+          userProfile.public_repos;
+        document.getElementById("ghFollowers").textContent =
+          userProfile.followers;
+        document.getElementById("ghFollowing").textContent =
+          userProfile.following;
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 404) {
+          document.getElementById("ghError").textContent =
+            "Username can't be found!";
+        } else {
+          document.getElementById("ghError").textContent =
+            "Something went wrong. Please try again.";
+        }
+      });
   }
 }
 
@@ -38,13 +48,13 @@ class GitHubRepo extends GitHubAPI {
       `https://api.github.com/users/${this.username}/repos`
     );
 
-    return response;
+    return response.data;
   }
 
   displayRepoData() {
     this.repo_container.innerHTML = "";
     this.getRepoData().then((userProfile) => {
-      userProfile.data.forEach((repo) => {
+      userProfile.forEach((repo) => {
         const li = document.createElement("li");
         li.textContent = repo.name;
         this.repo_container.appendChild(li);
@@ -61,6 +71,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (username.value === "") {
     document.getElementById("ghError").textContent = "Please enter username!";
+    return;
   }
   const userProfile = new GitHubAPI(username);
   const userRepo = new GitHubRepo(repoContainer, username);
